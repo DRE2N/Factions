ce76e87f66f6f407cee7f0225a8af4bf4391bb6d
Fyreum <luka.vogel@gmx.de>
2024-01-17 19:48:23 +0100

    I promise this is the last backup fix...

--- src/main/java/de/erethon/factions/Factions.java
+++ src/main/java/de/erethon/factions/Factions.java
@@ -388,7 +388,7 @@ public final class Factions extends EPlugin {
         File backupDir = new File(BACKUPS, String.valueOf(System.currentTimeMillis()));
         FileUtil.copyDir(getDataFolder(), backupDir, BACKUPS.getName(), "config.yml", "logger.yml");
 
-        List<File> backupList = FileUtil.getFilesForFolder(BACKUPS);
+        List<File> backupList = FileUtil.getFilesForFolder(backupDir);
         if (backupList.size() <= fConfig.getBackupsBeforeDeletion()) {
             return;
         }
