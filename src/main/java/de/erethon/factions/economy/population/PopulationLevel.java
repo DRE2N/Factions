ee727c1c3c87cd9ab6189b11a774687cb2197004
Malfrador <malfrador@gmail.com>
2024-02-18 20:22:33 +0100

    Add building level up requirements

--- src/main/java/de/erethon/factions/economy/population/PopulationLevel.java
+++ src/main/java/de/erethon/factions/economy/population/PopulationLevel.java
@@ -1,10 +1,17 @@
 package de.erethon.factions.economy.population;
 
+import de.erethon.factions.Factions;
+import de.erethon.factions.building.Building;
 import de.erethon.factions.economy.FStorage;
 import de.erethon.factions.economy.PopulationResourceConsumption;
 import de.erethon.factions.economy.resource.Resource;
+import de.erethon.factions.faction.Faction;
+import de.erethon.factions.util.FLogger;
+import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,27 +20,29 @@ import java.util.stream.Collectors;
  */
 public enum PopulationLevel {
 
-    BEGGAR(0),
-    PEASANT(0,
+    BEGGAR(new HashSet<>(),0),
+    PEASANT(new HashSet<>(), 0,
             new PopulationResourceConsumption(Resource.GRAIN, 0.5, 0, 3, 1.2),
             new PopulationResourceConsumption(Resource.FISH, 0.5, 0, 3, 0.5)),
-    CITIZEN(4,
+    CITIZEN(new HashSet<>(Arrays.asList("")),4,
             new PopulationResourceConsumption(Resource.MEAT, 0.7, 200, 1, 0.5),
             new PopulationResourceConsumption(Resource.BREAD, 0.7, 200, 5, 1.2),
             new PopulationResourceConsumption(Resource.FRUIT, 0.3, 200, 1, 0.5),
             new PopulationResourceConsumption(Resource.VEGETABLES, 0.3, 200, 1, 0.5)),
-    PATRICIAN(5,
+    PATRICIAN(new HashSet<>(Arrays.asList("")),5,
             new PopulationResourceConsumption(Resource.WINE, 1.0, 50, 5, 1.2),
             new PopulationResourceConsumption(Resource.BOOKS, 0.5, 200, 1, 0.5),
             new PopulationResourceConsumption(Resource.PAPER, 0.5, 200, 1, 0.5)),
-    NOBLEMEN(7,
+    NOBLEMEN(new HashSet<>(Arrays.asList("")), 7,
             new PopulationResourceConsumption(Resource.JEWELRY, 0.5, 200, 1, 0.5),
             new PopulationResourceConsumption(Resource.FURNITURE, 0.1, 10, 1, 0.5));
 
+    private final Set<String> buildings;
     private final int minimumVariety;
     private final Set<PopulationResourceConsumption> requiredResources;
 
-    PopulationLevel(int minimumVariety, PopulationResourceConsumption... requiredResources) {
+    PopulationLevel(Set<String> buildings, int minimumVariety, PopulationResourceConsumption... requiredResources) {
+        this.buildings = buildings;
         this.minimumVariety = minimumVariety;
         this.requiredResources = Set.of(requiredResources);
     }
@@ -76,6 +85,19 @@ public enum PopulationLevel {
 
     public Set<Resource> getResources() {
         return requiredResources.stream().map(PopulationResourceConsumption::resource).collect(Collectors.toSet());
+    }
+
+    public boolean hasRequiredBuildings(Faction faction) {
+        for (String id : buildings) {
+            if (!faction.hasBuilding(id)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Component displayName() {
+        return Component.translatable("factions.economy.population.level." + name().toLowerCase());
     }
 
 }
